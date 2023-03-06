d48a1627a4b943f8d84103653d4c36089ee74b46
Cagatay Unal <>
2023-03-06 14:39:41 +0300

    Create useDebounce Hook

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+import useDebounce from "./useDebounce";
+
+const DATA_LIST = [
+  "Luke Skywalker",
+  "C-3PO",
+  "R2-D2",
+  "Darth Vader",
+  "Leia Organa",
+  "Owen Lars",
+  "Beru Whitesun lars",
+  "R5-D4",
+  "Biggs Darklighter",
+  "Obi-Wan Kenobi",
+];
 
 function App() {
+  const [results, setResults] = useState([]);
+  const [text, setText] = useState("");
+
+  const debounce = useDebounce(text, 500);
+
+  useEffect(() => {
+    const data = DATA_LIST.filter((el) => el.toLowerCase().includes(debounce));
+    setResults(data);
+  }, [debounce]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <h1>search</h1>
+      <input
+        type="text"
+        value={text}
+        onChange={(e) => setText(e.target.value)}
+      />
+      <div className="search-result">
+        {results.length > 0 ? (
+          results.map((item, i) => (
+            <div key={i} className="result-item">
+              {item}
+            </div>
+          ))
+        ) : (
+          <p>No Result</p>
+        )}
+      </div>
     </div>
   );
 }
